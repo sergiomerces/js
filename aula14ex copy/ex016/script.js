5ce29c02a5e5e4e52c1c52edf5218fa417169cee
sergiomerces <sergio.merces@gmail.com>
2024-09-03 23:55:09 -0300

    validando entradas do exercicio 14

--- aula14ex copy/ex016/script.js
+++ aula14ex copy/ex016/script.js
@@ -4,14 +4,24 @@ function contar() {
     let passo = Number(document.querySelector('input#txtpasso').value);
     let resposta = document.querySelector('div#resposta');
 
-    resposta.innerHTML = ' Contando:<br>';
+    if(inicio === ''){
+        resposta.innerHTML = 'O início não pode ser vazio...';
+    } else if (fim === ''){
+        resposta.innerHTML = 'O fim não pode ser vazio...';
+    } else if(passo == ''){
+        resposta.innerHTML = 'O passo não pode ser vazio ou zero...';
+    } else if(inicio === fim){
+        resposta.innerHTML = 'Início e fim não podem ser o mesmo número...';
+    } else {
+        resposta.innerHTML = 'Contando:<br>';
 
-    while(inicio <= fim) {
-        resposta.innerHTML += `${inicio} \u{1F449}`;
-        inicio += passo;
+        while(inicio <= fim) {
+            resposta.innerHTML += `${inicio} \u{1F449}`;
+            inicio += passo;
+        }
+
+        resposta.innerHTML += `\u{1F3C1}`;
     }
-
-    resposta.innerHTML += `\u{1F3C1}`;
 }
 
 
